dae6c6dc1db01744fc0465ea2619f53e31f31ae2
artballesteros <58927442+artballesteros@users.noreply.github.com>
2021-10-01 22:40:32 -0700

    videos now play automatically for popular movies

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -50,7 +50,7 @@ public class DetailActivity extends YouTubeBaseActivity {
         try {
             applicationInfo = this.getPackageManager().getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA);
         } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
+            Log.e("Detail Activity", "Meta Data not retrieved", e);
         }
 
         if (applicationInfo != null) {
@@ -90,7 +90,11 @@ public class DetailActivity extends YouTubeBaseActivity {
                     @Override
                     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
                         youTubePlayer.cueVideo(youTubeKey);
+                        if (movie.getRating() > 5.0f) {
+                            youTubePlayer.loadVideo(youTubeKey);
+                        }
                     }
+
 
                     @Override
                     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
